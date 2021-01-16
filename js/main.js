4fb1b8ef13dcb2017cf10912a548b696235a3767
shibadisaster <38396090+shibadisaster@users.noreply.github.com>
2021-01-16 23:20:28 +0800

    Implement tab switching

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-console.log("balls");
+switch_tab("home");
 
 function switch_tab(tab) {
-    console.log(tab);
+    document.getElementById("content-home").style.display = "none";
+    document.getElementById("content-about-us").style.display = "none";
+    document.getElementById("content-cock-and-balls").style.display = "none";
+    document.getElementById("content-college-essay-completion-services").style.display = "none";
+
+    document.getElementById("content-" + tab).style.display = "";
+
+    document.getElementById("tab-home").classList.remove("active");
+    document.getElementById("tab-about-us").classList.remove("active");
+    document.getElementById("tab-cock-and-balls").classList.remove("active");
+    document.getElementById("tab-college-essay-completion-services").classList.remove("active");
+
+    document.getElementById("tab-" + tab).classList.add("active");
 }
