801f8e79fcff29045055527968111a35ecbedf11
Harvey Chua <harvs89.2@gmail.com>
2021-01-17 00:01:51 +0800

    Improved tab handling

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
+var active_tab = null
 function switch_tab(tab) {
-    document.getElementById("content-home").style.display = "none";
-    document.getElementById("content-about-us").style.display = "none";
-    document.getElementById("content-cock-and-balls").style.display = "none";
-    document.getElementById("content-college-essay-completion-services").style.display = "none";
-
-    document.getElementById("content-" + tab).style.display = "";
-
-    document.getElementById("tab-home").classList.remove("active");
-    document.getElementById("tab-about-us").classList.remove("active");
-    document.getElementById("tab-cock-and-balls").classList.remove("active");
-    document.getElementById("tab-college-essay-completion-services").classList.remove("active");
-
+    if(active_tab != null){
+        document.getElementById("content-" + active_tab).style.display = "none";
+        document.getElementById("tab-" + active_tab).classList.remove("active");
+    }
+    
+    document.getElementById("content-" + tab).style.display = "initial";
     document.getElementById("tab-" + tab).classList.add("active");
+    active_tab = tab
 }
 
 function ready() {
