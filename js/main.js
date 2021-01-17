2bbec5db34fbc32be96b58cf7608f48f5aabff07
Harvey Chua <harvs89.2@gmail.com>
2021-01-17 22:26:54 +0800

    Add UP Verification Code Generator FOR ENTERTAINMENT PURPOSES ONLY

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ function switch_tab(tab) {
 function ready() {
     switch_tab("home");
 }
+
+//https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function makeid() {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < 10; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   
+   document.getElementById("upcat-code").innerHTML = result;
+}
